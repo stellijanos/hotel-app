d59a9c716671328638c5ab81e0a984899ae0bce5
Janos Stelli <stellijanos23@gmail.com>
2024-04-06 11:44:58 +0300

    implement with api

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -34,9 +34,10 @@ export class ReservationFormComponent implements OnInit {
 		let id = this.activatedRoute.snapshot.paramMap.get('id');
 
 		if (id) {
-			let reservation = this.reservationService.getReservation(id);
-			if (reservation)
-				this.reservationForm.patchValue(reservation);
+			let reservation = this.reservationService.getReservation(id).subscribe(reservation => {
+				if (reservation)
+					this.reservationForm.patchValue(reservation);
+			});
 		}
 	}
 
@@ -47,9 +48,13 @@ export class ReservationFormComponent implements OnInit {
 			let id = this.activatedRoute.snapshot.paramMap.get('id');
 
 			if (id) {
-				this.reservationService.updateReservation(id, reservation);
+				this.reservationService.updateReservation(id, reservation).subscribe(() => {
+					console.log("Update request processed");
+				});
 			} else {
-				this.reservationService.addReservation(reservation);
+				this.reservationService.addReservation(reservation).subscribe(() => {
+					console.log("Create request processed");
+				});;
 			}
 			this.router.navigate(['/list']);
 		}
--- src/app/reservation-list/reservation-list.component.ts
+++ src/app/reservation-list/reservation-list.component.ts
@@ -17,11 +17,15 @@ export class ReservationListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.reservations = this.reservationService.getReservations();
+    this.reservationService.getReservations().subscribe(reservations => {
+      this.reservations = reservations;
+    });
   }
 
   deleteReservation(id: string) {
-    this.reservationService.deleteReservation(id);
+    this.reservationService.deleteReservation(id).subscribe(() => {
+      console.log("Delete request got processed");
+    });
   }
 
 
